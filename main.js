8f6964412c6ed5708b7c45e50b3f9d81e252b551
feddorovich <feddorovich@outlook.com>
2023-03-11 15:55:27 +0300

    menu done

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var menu = document.querySelector('.menu');
 var menuButton = document.querySelector('.menu-button');
+var closeButton = document.querySelector('.close-button');
 
 menuButton.addEventListener('click', function() {
     menu.classList.toggle('show');
@@ -9,4 +10,8 @@ document.addEventListener('click', function(event) {
     if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
         menu.classList.remove('show');
     }
+});
+
+closeButton.addEventListener('click', function() {
+    menu.classList.remove('show');
 });
